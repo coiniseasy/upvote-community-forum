033f4e51b8cb6c46b91bd8ab0b12695717f0bbfc
coiniseasy <coiniseasy@gmail.com>
2023-07-04 15:50:29 +0100

    cha

--- components/ArticleContent.jsx
+++ components/ArticleContent.jsx
@@ -114,7 +114,7 @@ export default function ArticleContent({post}) {
         {/** Show commenting feed only if not new post  */}
         {post.stream_id &&
           <div className="mt-6">
-            <Discussion context={post.content.context} master={post.stream_id} theme="kjzl6cwe1jw145s9pps4raggmjs7na2wmr61gddcjq7zrpplh7me27v1ekzmzmu" />
+            <Discussion context={post.content.context} master={post.stream_id} theme="kjzl6cwe1jw148kknvqhmo7gnvxa66n28gjhdbo0xv2p9omu00u0m3y2prxbk2c" />
           </div>
         }
 
--- pages/_app.js
+++ pages/_app.js
@@ -9,13 +9,13 @@ import { GlobalContext } from "../contexts/GlobalContext";
  * Set the global forum context here (you can create categories using the dashboard by clicking on "Create a sub-context"
  * from your main forum context)
  */
-global.orbis_context = "kjzl6cwe1jw14ae56dcglgwsify260a9je6ml9x7dut8gdlrm3j1105dyjfhg9u";
+global.orbis_context = "kjzl6cwe1jw1495msth5lqkr4aq9h4t9239ds8s40enjhyk9udx7zdxf0tdjo4g";
 
 /**
  * Set the global chat context here (the chat displayed when users click on the "Community Chat" button).
  * The Community Chat button will be displayed only if this variable is set
  */
-global.orbis_chat_context = "kjzl6cwe1jw14ae56dcglgwsify260a9je6ml9x7dut8gdlrm3j1105dyjfhg9u";
+global.orbis_chat_context = "kjzl6cwe1jw1495msth5lqkr4aq9h4t9239ds8s40enjhyk9udx7zdxf0tdjo4g";
 
 let orbis = new Orbis({
   useLit: false,
